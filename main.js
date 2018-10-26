118364244bebe60efa8076dcdcfa870b4dc4e8a9
Aliaksandr Bychak <aliaksandr.bychak@gmail.com>
2018-10-26 16:58:38 +0300

    add  script and animation

--- main.js
+++ main.js
@@ -28,9 +28,18 @@ document.addEventListener("DOMContentLoaded", ready);
 
 function ready() {
     let activeTabIndex = 0;
+
     let tabList = document.querySelectorAll('.tab');
+    let textList = document.querySelectorAll('.text-tab');
     let tabWrap = document.querySelector('.tabs-group');
     tabWrap.addEventListener('click', function(e){
-        console.log([...tabList].indexOf(e.target));
+        let currentTabIndex = [...tabList].indexOf(e.target);
+        if (currentTabIndex !== activeTabIndex){
+            tabList[currentTabIndex].classList.add('tab-active');
+            textList[currentTabIndex].classList.add('text-tab-active');
+            tabList[activeTabIndex].classList.remove('tab-active');
+            textList[activeTabIndex].classList.remove('text-tab-active');
+        }
+        activeTabIndex = currentTabIndex;
     })
   }
